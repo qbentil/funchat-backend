ec61a880ab92779aee5c8ba11237e09f39733868
qbentil <bentilshadrack72@gmail.com>
2022-11-30 14:25:43 +0000

    setup routes

--- server.ts
+++ server.ts
@@ -1,12 +1,19 @@
 import express, { Express, NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import DBCONNECT from "./db";
+import userRouter from "./routes/user.route";
+import ErrorHandler from "./middlewares/error";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const APP: Express = express();
+APP.use(express.json());
 
+APP.use("/user", userRouter)
+
+// Error Handler
+APP.use(ErrorHandler)
 
 APP.listen(PORT, () => {
     // connect to DB
