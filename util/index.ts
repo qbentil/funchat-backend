86513fa2cb03ef3039c940c35f6af26c35a660ae
qbentil <bentilshadrack72@gmail.com>
2022-11-30 14:27:03 +0000

    setup utils function

---
+++ util/index.ts
@@ -0,0 +1,21 @@
+import bcrypt from "bcryptjs";
+
+/**
+ * 
+ * @param l length of the PIN 
+ * @returns  an object with the PIN and the hash
+ */
+export const GeneratePIN = async (l?: number) => {
+    const len = l || 6;
+    // generate a random number with len digits
+    const random = Math.floor(Math.random() * Math.pow(10, len));
+    // pad the number with 0s to make it len digits long
+    const PIN = random.toString().padStart(len, "0");
+
+    // encrypt the PIN
+    const HASH = await bcrypt.hash(PIN, 10);
+
+    return { PIN, HASH }
+
+}
+
