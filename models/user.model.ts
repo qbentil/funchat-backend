45aa5a6ce7f9de804fa0d907beefa50fde2f3ad3
qbentil <bentilshadrack72@gmail.com>
2022-11-30 14:26:40 +0000

    create model

---
+++ models/user.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { model } from "mongoose";
+
+const USERSCHEMA = new mongoose.Schema({
+    username:{
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true,
+        minlength: [3, 'Username must be at least 3 characters long']
+    },
+    email:{
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+    },
+    avatar:{
+        type: String,
+        default: null 
+    },
+    password:{
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters long']
+    },
+    fullname:{
+        type: String,
+        default: null
+    },
+    token :{
+        type: String,
+        default: null
+    }
+}, {timestamps: true});
+
+export default mongoose.models.User || mongoose.model<any, any>('User', USERSCHEMA);
