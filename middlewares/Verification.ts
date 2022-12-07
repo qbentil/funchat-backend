dafbacb7c69276b0cc4a8e2072fe510db853f928
qbentil <bentilshadrack72@gmail.com>
2022-12-07 09:33:19 +0000

    Update Verification Flow

--- middlewares/Verification.ts
+++ middlewares/Verification.ts
@@ -1,24 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import CreateError from "../util/Error";
 declare module "express-serve-static-core" {
-	interface Request {
-		user: any;
+    interface Request {
+        user: any;
         token: string;
-	}
+    }
 }
 export const VerifyAccessToken = async (req: Request, res: Response, next: NextFunction) => {
     const { authorization } = req.headers;
-    if (!authorization) return next({ statusCode: 401, message: "Unauthorized" });
-    const token = authorization.split(" ")[1];
+    if (!authorization) return next(CreateError("Access token is required", 401));
+    const token: string = authorization.split(" ")[1];
     try {
-        const payload:any = jwt.verify(token, process.env.JWT_SECRET ||'');
-        if(!payload) return next({ statusCode: 401, message: "Invalid Access Token" });
+        const payload: any = jwt.verify(token, process.env.JWT_SECRET || '');
+        if (!payload) return next(CreateError("Invalid access token", 401));
 
         // check expiry
-        if (payload.exp < Date.now()/1000){
-            return next({ statusCode: 401, message: "Token has expired" })
+        if (payload.exp < Date.now() / 1000) {
+            return next(CreateError("Access token expired", 401))
         }
-        
+
         req.user = payload;
         req.token = token;
         next();
