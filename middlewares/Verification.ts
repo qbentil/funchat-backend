936e779e8be855c8afc26b96294c1f0ad1765c66
qbentil <bentilshadrack72@gmail.com>
2022-12-08 23:03:34 +0000

    Verify Refresh token

--- middlewares/Verification.ts
+++ middlewares/Verification.ts
@@ -27,3 +27,28 @@ export const VerifyAccessToken = async (req: Request, res: Response, next: NextF
         next(error);
     }
 }
+
+export const VerifyRefreshToken = async(req: Request, res: Response, next: NextFunction) => {
+    const {authorization } = req.headers;
+
+    // Throw error if not Auth token
+    if(!authorization) return next(CreateError("Unauthorized Access to Refresh", 401))
+
+    const token:string = authorization.split(" ")[1];
+
+    try{
+        const decoded:any = jwt.verify(token, process.env.JWT_REFRESH_SECRET || '')
+        if(!decoded) return next(CreateError("Invalid Refresh Token", 401))
+                // check expiry
+                if (payload.exp < Date.now() / 1000) {
+                    return next(CreateError("Access token expired", 401))
+                }
+        
+                req.user = payload;
+                req.token = token;
+                next();
+    }catch(e:any){
+        next(e)
+    }
+
+}
