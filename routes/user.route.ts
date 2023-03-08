ff11e721d4e8df7a07c2f1da521da107b88c3243
qbentil <bentilshadrack72@gmail.com>
2023-03-08 00:24:09 +0000

    Seperate auth route from user

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,14 +1,12 @@
 
 import { Router } from "express";
-import { getUsers, signUp } from "../controllers/user.controller";
+import { getUsers } from "../controllers/user.controller";
+
 
 const router = Router();
 
 // GET /user: get all users
 router.get("/", getUsers);
-
-// POST /user: create a new user
-router.post("/", signUp);
 
 
 export default router;
